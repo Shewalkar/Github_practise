825a6aca6879feaf753b8b3f2c4bf7ccca36dec9
Shewalkar <shewalkarshubham@gmail.com>
2024-10-03 21:36:34 +0530

    making it blank
    making it blank

--- simple.py
+++ simple.py
@@ -1,15 +1,1 @@
-<<<<<<< HEAD
-print("It's blank file")
-
-=======
-print("Shubham 1")
-print("Shubham 2")
-print("Shubham 3")
-print("Shubham 4")
-print("Shubham 5")
-print("Shubham 6")
-print("Shubham 7")
-print("Shubham 8")
-print("Shubham 9")
-print("Shubham 10")
->>>>>>> parent of 578d164 (filtered lines)
+print("it is a blank notebook")
